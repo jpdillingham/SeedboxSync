dc6c8deb5ac087d3e864b599298dc10660de64f5
JP Dillingham <jpdillingham@gmail.com>
2016-10-16 17:04:04 -0500

    General cleanup of Configuration, ConfigurationLoader and Server classes

--- src/WhatboxSync/IServer.java
+++ src/WhatboxSync/IServer.java
@@ -1,28 +1,103 @@
+/****************************************************************************
+ *
+ * IServer.java
+ *
+ * Defines the interface for Server objects.
+ *
+ ***************************************************************************
+ *
+ * Copyright (C) 2016 JP Dillingham (jp@dillingham.ws)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
+
 import java.util.List;
 import java.util.concurrent.Future;
 import java.io.File;
-
 import org.apache.commons.net.ftp.FTPFile;
 
 /**
  * Defines the interface for Server objects.
  */
 interface IServer {
-    /** Opens the Server connection. */
+    /**
+     * Gets the Server address.
+     * @return The Server address.
+     */
+    String getAddress();
+
+    /**
+     * Gets the username used when connecting to the Server.
+     * @return The username used when connecting to the Server.
+     */
+    String getUsername();
+
+    /**
+     * Gets the password used when connecting to the Server.
+     * @return The password used when connecting to the Server.
+     */
+    String getPassword();
+
+    /**
+     * Gets the Server port.
+     * @return The Server port.
+     */
+    Integer getPort();
+
+    /**
+     * Opens the Server connection using the specified IP address and the default port.
+     * @throws Exception Thrown if an exception is encountered during the connect or login operations.
+     */
     void connect() throws Exception;
 
-    /** Closes the Server connection. */
+    /**
+     * Closes the Server connection.
+     * @throws Exception Thrown if an exception is encountered during the disconnect operation.
+     */
     void disconnect() throws Exception;
 
-    /** Returns a value indicating whether the Server connection is open.
-     * @return */
+    /**
+     * Returns a value indicating whether the Server is connected.
+     * @return A value indicating whether the Server is connected.
+     */
     Boolean isConnected();
 
-    /** Lists the files in the specified directory. */
+    /**
+     * Returns a list of files contained within the specified directory.
+     * @param directory The directory for which to return the file list.
+     * @return A list of files contained within the directory.
+     * @throws Exception Thrown if an exception is encountered during the listFiles operation.
+     */
     List<FTPFile> list(String directory) throws Exception;
 
-    /** Downloads the specified file. */
+    /**
+     * Downloads the specified file.
+     * @param file The file to download.
+     * @param destinationFile The file to which the downloaded file should be saved.
+     * @return A value indicating whether the download completed successfully.
+     * @throws Exception Thrown if an exception is encountered during the download.
+     */
     Future<Boolean> download(String file, String destinationFile) throws Exception;
+
+    /**
+     * Downloads the specified file.
+     * @param file The file to download.
+     * @param destinationFile The file to which the downloaded file should be saved.
+     * @return A value indicating whether the download completed successfully.
+     * @throws Exception Thrown if an exception is encountered during the download.
+     */
     Future<Boolean> download(String file, File destinationFile) throws Exception;
 
 }
--- src/WhatboxSync/Server.java
+++ src/WhatboxSync/Server.java
@@ -1,14 +1,37 @@
-import java.io.FileOutputStream;
+/****************************************************************************
+ *
+ * Server.java
+ *
+ * Represents an FTP server.
+ *
+ ***************************************************************************
+ *
+ * Copyright (C) 2016 JP Dillingham (jp@dillingham.ws)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.Future;
 import java.io.File;
-
-
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
+import java.io.FileOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 
@@ -16,28 +39,43 @@ import org.springframework.scheduling.annotation.AsyncResult;
  * Represents an FTP server.
  */
 public class Server implements IServer {
-    /** The logger for this class. */
+    /**
+     * The logger for this class.
+     */
     Logger logger = LoggerFactory.getLogger(Server.class);
 
-    /** The default port. */
+    /**
+     * The default port.
+     */
     private static final Integer defaultPort = 21;
 
-    /** The FTPClient driving the server */
+    /**
+     * The FTPClient driving the server
+     */
     private static FTPClient server;
 
-    /** The Server address. */
+    /**
+     * The Server address.
+     */
     private String address;
 
-    /** The Server port. */
+    /**
+     * The Server port.
+     */
     private Integer port;
 
-    /** The username to use when connecting to the Server. */
+    /**
+     * The username to use when connecting to the Server.
+     */
     private String username;
 
-    /** The password to use when connecting to the Server. */
+    /**
+     * The password to use when connecting to the Server.
+     */
     private String password;
 
-    /** Initializes a new instance of the Server class with the specified IP address, username and password.
+    /**
+     * Initializes a new instance of the Server class with the specified IP address, username and password.
      * @param address The Server address.
      * @param username The username to use when connecting to the Server.
      * @param password The password to use when connecting to the Server.
@@ -46,7 +84,8 @@ public class Server implements IServer {
         this(address, username, password, defaultPort);
     }
 
-    /** Initializes a new instance of the Server class with the specified IP address, username, password and port.
+    /**
+     * Initializes a new instance of the Server class with the specified IP address, username, password and port.
      * @param address The Server address.
      * @param username The username to use when connecting to the Server.
      * @param password The password to use when connecting to the Server.
@@ -62,35 +101,42 @@ public class Server implements IServer {
         this.server = new FTPClient();
     }
 
-    /** Gets the Server address.
+    /**
+     * Gets the Server address.
      * @return The Server address.
      */
     public String getAddress() {
         return address;
     }
 
-    /** Gets the username used when connecting to the Server.
+    /**
+     * Gets the username used when connecting to the Server.
      * @return The username used when connecting to the Server.
      */
     public String getUsername() {
         return username;
     }
 
-    /** Gets the password used when connecting to the Server.
+    /**
+     * Gets the password used when connecting to the Server.
      * @return The password used when connecting to the Server.
      */
     public String getPassword() {
         return password;
     }
 
-    /** Gets the Server port.
+    /**
+     * Gets the Server port.
      * @return The Server port.
      */
     public Integer getPort() {
         return port;
     }
 
-     /** Opens the Server connection using the specified IP address and the default port. */
+     /**
+      * Opens the Server connection using the specified IP address and the default port.
+      * @throws Exception Thrown if an exception is encountered during the connect or login operations.
+      */
     public void connect() throws Exception {
         logger.debug("Connecting to '" + address + "'...");
         server.connect(address, port);
@@ -99,22 +145,28 @@ public class Server implements IServer {
         server.login(username, password);
     }
 
-    /** Closes the Server connection. */
+    /**
+     * Closes the Server connection.
+     * @throws Exception Thrown if an exception is encountered during the disconnect operation.
+     */
     public void disconnect() throws Exception {
         logger.debug("Disconnecting from '" + address + "'...");
         server.disconnect();
     }
 
-    /** Returns a value indicating whether the Server is connected.
+    /**
+     * Returns a value indicating whether the Server is connected.
      * @return A value indicating whether the Server is connected.
      */
     public Boolean isConnected() {
         return server.isConnected();
     }
 
-    /** Returns a list of files contained within the specified directory.
+    /**
+     * Returns a list of files contained within the specified directory.
      * @param directory The directory for which to return the file list.
      * @return A list of files contained within the directory.
+     * @throws Exception Thrown if an exception is encountered during the listFiles operation.
      */
     public List<FTPFile> list(String directory) throws Exception {
         List<FTPFile> retVal = new ArrayList<FTPFile>();
@@ -130,7 +182,8 @@ public class Server implements IServer {
         return retVal;
     }
 
-    /** Downloads the specified file.
+    /**
+     * Downloads the specified file.
      * @param file The file to download.
      * @param destinationFile The file to which the downloaded file should be saved.
      * @return A value indicating whether the download completed successfully.
@@ -142,8 +195,8 @@ public class Server implements IServer {
         return new AsyncResult<Boolean>(true);
     }
 
-
-    /** Downloads the specified file.
+    /**
+     * Downloads the specified file.
      * @param file The file to download.
      * @param destinationFile The file to which the downloaded file should be saved.
      * @return A value indicating whether the download completed successfully.
