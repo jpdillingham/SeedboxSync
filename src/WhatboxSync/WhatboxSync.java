596f143cb549df47cb7b65d6cd7ba56efa886542
JP Dillingham <jpdillingham@gmail.com>
2016-10-20 18:57:35 -0500

    General refactoring of main class

--- src/WhatboxSync/WhatboxSync.java
+++ src/WhatboxSync/WhatboxSync.java
@@ -34,50 +34,44 @@ import org.slf4j.LoggerFactory;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
 
-/** The main application class. */
+/**
+ * The main application class.
+ */
 public class WhatboxSync {
     /**
      * The logger for this class.
      */
     private static Logger logger = LoggerFactory.getLogger(new Throwable().getStackTrace()[0].getClassName());
 
-    /** The IConfiguraiton instance containing the application configuratiom. */
+    /**
+     * The IConfiguraiton instance containing the application configuratiom.
+     */
     private static IConfiguration config;
 
-    /** The ISynchronizer instance containing the Synchronizer for the application. */
-    private static ISynchronizer syncher;
+    /**
+     * The ISynchronizer instance containing the Synchronizer for the application.
+     */
+    private static ISynchronizer synchronizer;
 
-    /** The ScheduledExecutaorService instance which handles scheduled synchronization. */
+    /**
+     * The ScheduledExecutaorService instance which handles scheduled synchronization.
+     */
     private static ScheduledExecutorService service;
 
-    /** The main entry point for the application.
-     * @param args The command-line arguments passed to the application when starting. */
+    /**
+     * The main entry point for the application.
+     * @param args The command-line arguments passed to the application when starting.
+     */
     public static void main(String[] args) {
-        String configFile = System.getProperty("user.dir") + File.separator + "config.json";
-
-	    logger.info("Retrieving configuration from '" + configFile + "'...");
-
-        // attempt to find and load the configuration file.
-        // the file (config.json) needs to be located in the root of the application directory.
-        if ((new File(configFile)).exists()) {
-            try {
-                config = new ConfigurationLoader().load(configFile);
-
-                logger.info("Configuration retrieved successfully.");
-            } catch (Exception ex) {
-                logger.error("Error retrieving configuration: " + ex.getMessage());
-            }
-        }
-        else {
-            logger.error("Configuration file '" + configFile + "' was not found.");
-        }
+        configureLogging();
+        config = loadConfiguration();
 
         logger.info("Creating a Synchronizer...");
 
         // instantiate a Synchronizer with the fetched configuration
         try {
             IServer server = new ServerFactory().createServer(config);
-            syncher = new Synchronizer(config, server, new Database());
+            synchronizer = new Synchronizer(config, server, new Database());
         }
         catch (Exception ex) {
             logger.error("Error creating Synchronizer: " + ex.getMessage());
@@ -91,21 +85,48 @@ public class WhatboxSync {
             public void run() {
                 logger.info("Starting Synchronization...");
                 try {
-                    syncher.synchronize();
+                    synchronizer.synchronize();
                 } catch (Exception ex) {  }
             }
         }, 0, config.getInterval(), TimeUnit.SECONDS);
     }
 
+    /**
+     * Configures the logger.
+     */
     private static void configureLogging() {
-        ConsoleAppender console = new ConsoleAppender(); //create appender
-        //configure the appender
-        String PATTERN = "%d{yyyy-MM-dd' 'HH:mm:ss.SSS} [%-5p] [%c] - %m%n";
-        console.setLayout(new PatternLayout(PATTERN));
+        ConsoleAppender console = new ConsoleAppender();
+        console.setLayout(new PatternLayout("%d{yyyy-MM-dd' 'HH:mm:ss.SSS} [%-5p] [%c] - %m%n"));
         console.setThreshold(Level.INFO);
         console.activateOptions();
-
-        //add appender to any Logger (here is root)
         org.apache.log4j.Logger.getRootLogger().addAppender(console);
+    }
+
+    /**
+     * Loads the application configuration from file.
+     */
+    private static Configuration loadConfiguration() {
+        Configuration config;
+        String configFile = System.getProperty("user.dir") + File.separator + "config.json";
+
+        logger.info("Retrieving configuration from '" + configFile + "'...");
+
+        // attempt to find and load the configuration file.
+        // the file (config.json) needs to be located in the root of the application directory.
+        if ((new File(configFile)).exists()) {
+            try {
+                config = new ConfigurationLoader().load(configFile);
+
+                logger.info("Configuration retrieved successfully.");
+
+                return config;
+            } catch (Exception ex) {
+                logger.error("Error retrieving configuration: " + ex.getMessage());
+            }
+        }
+        else {
+            logger.error("Configuration file '" + configFile + "' was not found.");
+        }
+        return null;
     }
 }
